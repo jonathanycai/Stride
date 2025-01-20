6d7e9cf8dc6a1a668bc61999b9b01228f608ecbf
Jonathan Cai <jonathan.y.cai@outlook.com>
2025-01-20 00:07:39 -0800

    Created visualization for header bar in CurrentWorkoutView

---
+++ Stride/View/AppColour.swift
@@ -0,0 +1,13 @@
+//
+//  AppColour.swift
+//  Stride
+//
+//  Created by Jonathan Cai on 2025-01-15.
+//
+
+import SwiftUI
+
+struct AppColour {
+    static let main = Color(red: 66/255, green: 65/255, blue: 67/255)
+    static let headerText = Color(red: 221/255, green: 218/255, blue: 218/255)
+}
--- Stride/View/CurrentWorkoutView.swift
+++ Stride/View/CurrentWorkoutView.swift
@@ -9,7 +9,37 @@ import SwiftUI
 
 struct CurrentWorkoutView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            AppColour.main.ignoresSafeArea()
+            //header
+            VStack(alignment: .leading, spacing: 8) {
+                HStack(spacing: 16) {
+                    ZStack {
+                        Circle()
+                            .fill(AppColour.headerText) // Background color for the circle
+                            .frame(width: 50, height: 50) // Size of the circle
+                        Text("JG")
+                            .font(.headline)
+                            .fontWeight(.bold)
+                            .foregroundColor(AppColour.main) // Text color
+                    }
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Good Morning!")
+                            .font(.headline)
+                            .fontWeight(.bold)
+                        Text("Hello, John Grey")
+                            .font(.subheadline)
+                    }
+                }
+                Divider()
+                    .background(AppColour.headerText)
+            }
+            .padding()
+            .foregroundColor(AppColour.headerText)
+        }
+        
+        
     }
 }
 
