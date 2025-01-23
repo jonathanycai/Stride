5d6f0d43bb7fe50bee03c1afe87f70d023010968
Jonathan Cai <jonathan.y.cai@outlook.com>
2025-01-23 13:07:08 -0800

    Resolved NavBar issue by making it the content view

---
+++ Stride/View/StrideAnimation.swift
@@ -0,0 +1,18 @@
+//
+//  StrideAnimation.swift
+//  Stride
+//
+//  Created by Jonathan Cai on 2025-01-23.
+//
+
+import SwiftUI
+
+struct StrideAnimation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StrideAnimation()
+}
