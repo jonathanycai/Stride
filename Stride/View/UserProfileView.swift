2eb6db8b2a002b628177cb16f8ffe64959cede3a
Jonathan Cai <jonathan.y.cai@outlook.com>
2025-01-24 12:44:25 -0800

    Create UserProfileView

---
+++ Stride/View/UserProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfileView.swift
+//  Stride
+//
+//  Created by Jonathan Cai on 2025-01-24.
+//
+
+import SwiftUI
+
+struct UserProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserProfileView()
+}
