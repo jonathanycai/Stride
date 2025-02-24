9d934b2d637c1745cee3199cab5b4573143af068
Jonathan Cai <jonathan.y.cai@outlook.com>
2025-02-24 08:19:27 -0800

    Created small home page to complete basic workflow

--- Stride/View/HomePageView.swift
+++ Stride/View/HomePageView.swift
@@ -9,10 +9,63 @@ import SwiftUI
 
 struct HomePageView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            AppColour.main
+                .ignoresSafeArea()
+            
+            ScrollView {
+                
+                VStack(alignment: .leading, spacing: 24) {
+                    // HEADER
+                    VStack(alignment: .leading, spacing: 8) {
+                        HStack(spacing: 16) {
+                            ZStack {
+                                Circle()
+                                    .fill(AppColour.headerText)
+                                    .frame(width: 50, height: 50)
+                                Text("JG")
+                                    .font(.headline)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(AppColour.main)
+                            }
+                            
+                            VStack(alignment: .leading, spacing: 4) {
+                                Text("Good Morning!")
+                                    .font(.headline)
+                                    .fontWeight(.bold)
+                                Text("Hello, John Grey")
+                                    .font(.subheadline)
+                            }
+                        }
+                        Divider()
+                            .background(AppColour.headerText)
+                        
+                        
+                    }
+                    .padding(.horizontal)
+                    .foregroundColor(AppColour.headerText)
+                 
+                    Spacer()
+                    
+                    NavigationLink(destination: NewWorkoutView()) {
+                        Text("GET STARTED")
+                            .font(.headline)
+                            .foregroundColor(.black)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(AppColour.lightButtonBackground)
+                            .cornerRadius(15)
+                            .padding(.horizontal)
+                    }
+                    .padding(.bottom)
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    HomePageView()
+    NavigationView {
+        HomePageView()
+    }
 }
